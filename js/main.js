48ea3530371f7a2087a27262f8b0edf43b1914d4
Herut Yona <yonaherut@gmail.com>
2018-06-19 09:37:20 +0300

    some modifications

--- js/main.js
+++ js/main.js
@@ -7,14 +7,6 @@ var gProjects = [{
     "url": "projs/sokoban",
     "publishedAt": 1448693940000,
     "labels": ["Matrixes", "keyboard events"],
-}, {
-    "id": "sokoban",
-    "name": "Sokoban",
-    "title": "Better push those boxes",
-    "desc": "lorem ipsum lorem ipsum lorem ipsum",
-    "url": "projs/sokoban",
-    "publishedAt": 1448693940000,
-    "labels": ["Matrixes", "keyboard events"],
 }]
 
 
